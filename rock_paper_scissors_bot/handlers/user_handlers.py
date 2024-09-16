9667c9056f943f1468fe7ca7bd01ad6ebbcce18a
nevek12 <144271001+nevek12@users.noreply.github.com>
2024-09-16 18:51:49 +0300

    Add files via upload

---
+++ rock_paper_scissors_bot/handlers/user_handlers.py
@@ -0,0 +1,34 @@
+from aiogram import Router, Bot
+from aiogram.types import Message
+from aiogram.filters import Command, CommandStart, Text
+from keyboards.keyboards import game_kb, yes_no_kb
+from lexicon.lexicon_ru import LEXICON_RU
+from services.services import get_bot_choice, get_winner
+from config_data.config import load_config
+router: Router = Router()
+bot: Bot = Bot(token='5823767957:AAHHq1hltBdA3W10_hrzm2fmV40OZjSSvpo')
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'], reply_markup=yes_no_kb)
+@router.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'], reply_markup=yes_no_kb)
+@router.message(Text(text=LEXICON_RU['yes_button']))
+async def process_yes_answer(message: Message):
+    await message.answer(text=LEXICON_RU['yes'], reply_markup=game_kb)
+@router.message(Text(text=LEXICON_RU['no_button']))
+async def process_no_answer(message: Message):
+    await message.answer(text=LEXICON_RU['no'])
+@router.message(Text(text=[LEXICON_RU['rock'],
+                           LEXICON_RU['paper'],
+                           LEXICON_RU['scissors']]))
+async def process_game_button(message: Message):
+    bot_choice = get_bot_choice()
+    await message.answer(text=f'{LEXICON_RU["bot_choice"]}'
+                              f'- {LEXICON_RU[bot_choice]}')
+    winner = get_winner(message.text, bot_choice)
+    await message.answer(text=LEXICON_RU[winner], reply_markup=yes_no_kb)
+@router.message(Command(commands='delmenu'))
+async def del_main_menu(message: Message, bot: Bot):
+    await bot.delete_my_commands()
+    await message.answer(text='Кнопка "Menu" удалена')
